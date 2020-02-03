0db7e34c00dcb7c762d4f6bdfa4827eee5f3a0f4
Ethan Green <egreen18@rams.colostate.edu>
2020-02-03 15:41:01 -0700

    Updated functions and data storage for compatibility with new function.

--- ODPlot.m
+++ ODPlot.m
@@ -1,18 +1,16 @@
 %Ethan Green
 %February 2nd, 2020
-function ODPlot(t,OD,n,key)
-%Plots OD reading data for any number of concurrent growth samples
-%Takes inputs t, the vector of all OD sample times; OD, the matrix of all
-%OD absorbance readings; n, the number of concurrent growth samples, and
-%key, the cell of all sample names
-plot(t,OD(1,:))
+function ODPlot(batch)
+%Plots OD reading data for any number of concurrent growth samples.
+%Takes input batch, the structure of all data for the batch.
+plot(batch.t,batch.OD(1,:))
 hold on
-leg = strings(1,n);
-leg(1) = key{1};
-if n>1
-    for i = 2:n
-    plot(t,OD(i,:))
-    leg(i) = key{i};
+leg = strings(1,batch.n);
+leg(1) = batch.key{1};
+if batch.n>1
+    for i = 2:batch.n
+    plot(batch.t,batch.OD(i,:))
+    leg(i) = batch.key{i};
     end
 end
     legend(leg)
