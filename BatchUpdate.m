1921b603ba196a93459a4673a0073c04cfa331c0
Ethan Green <egreen18@rams.colostate.edu>
2020-02-03 15:37:24 -0700

    New functions calls on prior functions to expedite btach data update process

---
+++ BatchUpdate.m
@@ -0,0 +1,16 @@
+%Ethan Green
+%February 3rd, 2020
+function BatchUpdate(batch)
+%Updates the batch structure and displays using ODPlot.
+%Takes input batch, the structure of all data from the batch.
+%Initializing
+    disp("Updating data from the "+batch.title+" experiment.")
+%Updating t vector
+    batch = ODTUpdate(batch);
+%Updating OD matrix
+    batch = ODUpdate(batch);
+%Exporting data to workspace
+    assignin('base','batch',batch);
+%Plotting for visualization of updates
+    ODPlot(batch);
+end
