50f085f539077c1cee2a408d65dce14d3abdf333
Ethan Green <egreen18@rams.colostate.edu>
2020-02-03 00:11:12 -0700

    Allowed for function to interact with variable workspace

--- ODTUpdate.m
+++ ODTUpdate.m
@@ -1,4 +1,9 @@
+%Ethan Green
+%February 2nd, 2020
 function t = ODTUpdate(ti,t)
+%Updates the t vector for usage in the ODPlot function
+%Takes input ti, the time of the first OD reading; and t, the current
+%vector of OD recording times
 tn = input("What time were the readings taken? [hour, minute, month, day, year]");
 tnstring = {num2str(tn(1)), num2str(tn(2)), num2str(tn(3)), num2str(tn(4)), num2str(tn(5))};
 tistring = {num2str(ti(1)), num2str(ti(2)), num2str(ti(3)), num2str(ti(4)), num2str(ti(5))};
@@ -12,6 +17,7 @@ Ti = datetime(tidate{1});
 dT = Tn - Ti;
 l = length(t);
 t(l+1) = hours(dT);
+assignin('base','t',t);
 end
 
 
